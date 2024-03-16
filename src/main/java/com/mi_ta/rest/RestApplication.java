b8d439d24127bddc2f79b4fbaafd6c5e8347b1e7
tatsuya <mita.gcp@gmail.com>
2024-03-17 00:18:37 +0900

    first commit

---
+++ src/main/java/com/mi_ta/rest/RestApplication.java
@@ -0,0 +1,9 @@
+package com.mi_ta.rest;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class RestApplication extends Application {
+
+}
